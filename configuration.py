b4e7f607f550d4d8c0977bde71c65e10be31adcf
Axel Wettervik <axel.wettervik@gmail.com>
2023-05-10 18:24:41 +0900

    feat: add configuration class

---
+++ configuration.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+class Configuration:
+    """App configuration variables"""
+
+    def __init__(self):
+        load_dotenv()
+
+        self.discord_token = os.getenv('DISCORD_TOKEN')
+        self.discord_channel = os.getenv('DISCORD_CHANNEL')
+        self.alliance_id = os.getenv('ALLIANCE_ID')
+
+    def __str__(self):
+        return '{}(discord_token={}, discord_channel={}, alliance_id={})'.format(
+            self.__class__.__name__, self.discord_token, self.discord_channel, self.alliance_id
+        )
