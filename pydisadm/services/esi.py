32cb6fa4eb3dd5482a513caf45f4e2631fb68250
Axel Wettervik <axel.wettervik@gmail.com>
2023-05-17 19:24:35 +0900

    fix: linting issues in esi and datetime_utils

--- pydisadm/services/esi.py
+++ pydisadm/services/esi.py
@@ -4,6 +4,8 @@ import requests
 
 logger = logging.getLogger('esi_api')
 
+BASE_ENDPOINT="https://esi.evetech.net/latest"
+
 def safe_get(url):
     """Safe get request"""
     try:
@@ -28,29 +30,29 @@ def safe_post(url, json):
 
 def universe_names(ids):
     """Get names from IDs"""
-    url = "https://esi.evetech.net/latest/universe/names/?datasource=tranquility"
+    url = f"{BASE_ENDPOINT}/universe/names/?datasource=tranquility"
     return safe_post(url, ids).json()
 
 
 def universe_system(system_id):
     """Get system by ID"""
-    url = f"http://esi.evetech.net/latest/universe/systems/{system_id}?datasource=tranquility"
+    url = f"{BASE_ENDPOINT}/universe/systems/{system_id}?datasource=tranquility"
     return safe_get(url).json()
 
 
 def universe_constellation(constellation_id):
     """Get constellation by ID"""
-    url = f"http://esi.evetech.net/latest/universe/constellations/{constellation_id}?datasource=tranquility"
+    url = f"{BASE_ENDPOINT}/universe/constellations/{constellation_id}?datasource=tranquility"
     return safe_get(url).json()
 
 
 def universe_region(region_id):
     """Get region by ID"""
-    url = f"http://esi.evetech.net/latest/universe/regions/{region_id}?datasource=tranquility"
+    url = f"{BASE_ENDPOINT}/universe/regions/{region_id}?datasource=tranquility"
     return safe_get(url).json()
 
 
 def sovereignty_structures():
     """Get sovereignty structures"""
-    url = "https://esi.evetech.net/latest/sovereignty/structures/?datasource=tranquility"
+    url = f"{BASE_ENDPOINT}/sovereignty/structures/?datasource=tranquility"
     return safe_get(url).json()
