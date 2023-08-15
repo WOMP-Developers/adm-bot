497fd2675c57ddc3d5e7d5b904b082c8083348c9
Axel Wettervik <axel.wettervik@gmail.com>
2023-08-15 13:13:29 +0900

    fix: rollback mysql on OperationalError
    The bot's been struggling in production environment because of Mysql
    connection dropping during queries in some situations. This triggers
    consistency errors in sqlalchemy because there's no rollback when the
    error happens. This commit will rollback if any OperationalError is
    raised and then it will raise the same error to fail the bot command.

--- pydisadm/services/database_mysql.py
+++ pydisadm/services/database_mysql.py
@@ -1,5 +1,7 @@
 """Mysql database service"""
+import logging
 from sqlalchemy import create_engine, text
+from sqlalchemy.exc import OperationalError
 import pandas as pd
 
 from pydisadm.services.common import (
@@ -7,11 +9,13 @@ from pydisadm.services.common import (
 )
 from pydisadm.services.database import Database
 
+logger = logging.getLogger('database_mysql')
+
 class DatabaseMysql(Database):
     """Mysql database service implementation"""
 
     def __init__(self, conn_string):
-        self.engine = create_engine(f'mysql+mysqldb://{conn_string}', pool_size=5)
+        self.engine = create_engine(f'mysql+mysqldb://{conn_string}', pool_size=5, echo_pool=True)
         self.setup()
 
     def setup(self):
@@ -24,31 +28,69 @@ class DatabaseMysql(Database):
 
     def insert_systems(self, systems):
         """Insert system ADM records"""
-        systems.to_sql('adm', self.engine, index=False, if_exists='append')
-        systems.to_sql('adm_history', self.engine, index=False, if_exists='append')
+        with self.engine.connect() as conn:
+            try:
+                systems.to_sql('adm', conn, index=False, if_exists='append')
+                systems.to_sql('adm_history', conn, index=False, if_exists='append')
+            except OperationalError as error:
+                logger.error('insert_systems error', exc_info=error)
+                conn.rollback()
+                raise error
 
     def insert_map_data(self, map_data):
         """Insert map data"""
-        map_data.to_sql('map', self.engine, index=True, if_exists='replace')
+        with self.engine.connect() as conn:
+            try:
+                map_data.to_sql('map', conn, index=True, if_exists='replace')
+            except OperationalError as error:
+                logger.error('insert_map_data error', exc_info=error)
+                conn.rollback()
+                raise error
 
     def select_system_with_name(self, system_name) -> pd.DataFrame:
         """Select system matching name"""
-        return pd.read_sql_query(text("SELECT * FROM map WHERE solarSystemName = :system_name"),
-                                 self.engine, params={ 'system_name': system_name })
+        with self.engine.connect() as conn:
+            try:
+                system = pd.read_sql_query(
+                    text("SELECT * FROM map WHERE solarSystemName = :system_name"),
+                    conn, params={ 'system_name': system_name })
+            except OperationalError as error:
+                logger.error('select_system_with_name error', exc_info=error)
+                conn.rollback()
+                raise error
+
+        return system
 
     def select_most_recent_row(self) -> pd.DataFrame:
         """Select the most recent ADM record"""
-        return pd.read_sql_query(text("""
-            SELECT created_at FROM adm ORDER BY created_at DESC LIMIT 1
-        """), self.engine)
+        with self.engine.connect() as conn:
+            try:
+                most_recent = pd.read_sql_query(text("""
+                    SELECT created_at FROM adm ORDER BY created_at DESC LIMIT 1
+                """), conn)
+            except OperationalError as error:
+                logger.error('select_most_recent_row error', exc_info=error)
+                conn.rollback()
+                raise error
+
+        return most_recent
 
     def select_systems(self) -> pd.DataFrame:
         """Select most recent record of all systems"""
-        return pd.read_sql_query(text("""
-            SELECT t1.*, t2.solarSystemName, t2.constellationName, t2.regionName FROM adm t1 
-            LEFT JOIN map t2 ON t1.system_id = t2.solarSystemID 
-            WHERE t1.created_at = (SELECT MAX(t3.created_at) FROM adm t3 WHERE t3.system_id = t1.system_id);
-        """), self.engine)
+
+        with self.engine.connect() as conn:
+            try:
+                systems = pd.read_sql_query(text("""
+                    SELECT t1.*, t2.solarSystemName, t2.constellationName, t2.regionName FROM adm t1 
+                    LEFT JOIN map t2 ON t1.system_id = t2.solarSystemID 
+                    WHERE t1.created_at = (SELECT MAX(t3.created_at) FROM adm t3 WHERE t3.system_id = t1.system_id);
+                """), conn)
+            except OperationalError as error:
+                logger.error('select_systems error', exc_info=error)
+                conn.rollback()
+                raise error
+
+        return systems
 
     def select_system_by_name(self, name) -> pd.DataFrame:
         """Select systems by name"""
@@ -59,23 +101,45 @@ class DatabaseMysql(Database):
                 map.constellationName=:constellation_name OR
                 map.regionName=:region_name ORDER BY created_at
         """)
-        return pd.read_sql_query(sql, self.engine, params={ 'system_name': name,
-                                 'constellation_name': name, 'region_name': name })
+        with self.engine.connect() as conn:
+            try:
+                system = pd.read_sql_query(sql, conn, params={ 'system_name': name,
+                                        'constellation_name': name, 'region_name': name })
+            except OperationalError as error:
+                logger.error('select_system_by_name error', exc_info=error)
+                conn.rollback()
+                raise error
+
+        return system
 
     def select_system_history(self, system, limit) -> pd.DataFrame:
         """Select history of a single system"""
-        return pd.read_sql_query(text("""
-            SELECT system_id, adm, tier, created_at FROM adm
-            INNER JOIN map ON map.solarSystemID = systems.system_id
-            WHERE map.solarSystemName=:system_name ORDER BY created_at DESC LIMIT :limit
-        """), self.engine, params={'system_name': system, 'limit': limit })
+
+        with self.engine.connect() as conn:
+            try:
+                system_history = pd.read_sql_query(text("""
+                    SELECT system_id, adm, tier, created_at FROM adm
+                    INNER JOIN map ON map.solarSystemID = systems.system_id
+                    WHERE map.solarSystemName=:system_name ORDER BY created_at DESC LIMIT :limit
+                """), conn, params={'system_name': system, 'limit': limit })
+            except OperationalError as error:
+                logger.error('select_system_history error', exc_info=error)
+                conn.rollback()
+                raise error
+
+        return system_history
 
     def delete_system_rows(self, days_old):
         """Delete system rows older than days_old"""
 
         with self.engine.connect() as conn:
-            conn.execute(text("""
-                DELETE FROM adm WHERE created_at < DATE(NOW()-INTERVAL :days DAY)
-            """), parameters={ 'days': days_old })
+            try:
+                conn.execute(text("""
+                    DELETE FROM adm WHERE created_at < DATE(NOW()-INTERVAL :days DAY)
+                """), parameters={ 'days': days_old })
 
-            conn.commit()
+                conn.commit()
+            except OperationalError as error:
+                logger.error('delete_system_rows error', exc_info=error)
+                conn.rollback()
+                raise error
